af5f457e9f052d9b381cc293c545541a030c282a
Elad Or <elad725@gmail.com>
2021-08-24 19:15:45 +0300

    Can switch between songs from library
    needs to fix pause/play icon when switching - state lifting the state of the icon and passing it to LibrarySong could work.

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, id }) => {
+
 	return (
-		<div className="library-song">
+		<div onClick={ () => setCurrentSong(song) } className="library-song">
 			<img src={song.cover} alt={song.name} />
 			<div className="song-description">
 				<h2>{song.name}</h2>
--- src/components/Player.js
+++ src/components/Player.js
@@ -5,9 +5,14 @@ import { faPlay, faAngleLeft, faAngleRight, faPause } from "@fortawesome/free-so
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 	const [ playIcon, setPlayIcon ] = useState(faPlay);
 
-	//Ref
+	//*Ref
 	const audioRef = useRef(null);
-	//Event Handlers
+	//*State
+	const [ songInfo, setSongInfo ] = useState({
+		currentTime: 0,
+		duration: 0
+	});
+	//*Event Handlers
 	const playSongHandler = () => {
 		if (isPlaying) {
 			setPlayIcon(faPause);
@@ -21,7 +26,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 	};
 
 	const TimeHandler = (e) => {
-		const current = e.target.currentTime;
+		const current = e.target.currentTime; //?-- to keep it simple when adding to SongInfo
 		const duration = e.target.duration;
 		setSongInfo({ ...songInfo, currentTime: current, duration: duration }); //...songInfo is 'keep previous info'
 	};
@@ -36,18 +41,9 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 		setSongInfo({ ...songInfo, currentTime: e.target.value }); //keep info and update current time
 	};
 
-	const nextHandler = (e) => {
-		
-	}
+	const nextHandler = (e) => {};
 
-	const prevHandler = (e) => {
-
-	}
-	//State
-	const [ songInfo, setSongInfo ] = useState({
-		currentTime: 0,
-		duration: 0
-	});
+	const prevHandler = (e) => {};
 
 	return (
 		<div className="player">
@@ -66,7 +62,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
 			<div className="play-control">
 				<FontAwesomeIcon onClick={prevHandler} className="skip-back" icon={faAngleLeft} size="2x" />
 				<FontAwesomeIcon onClick={playSongHandler} className="play" icon={playIcon} size="2x" />
-				<FontAwesomeIcon onClick ={nextHandler} className="skip-forward" icon={faAngleRight} size="2x" />
+				<FontAwesomeIcon onClick={nextHandler} className="skip-forward" icon={faAngleRight} size="2x" />
 			</div>
 			<audio onLoadedMetadata={TimeHandler} onTimeUpdate={TimeHandler} ref={audioRef} src={currentSong.audio} />
 		</div>
